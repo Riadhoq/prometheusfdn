3f61ca0994b5546bc65fe04e2c1c30041d0af116
Riadul Hoque <hoqueriadul@gmail.com>
2019-12-04 23:48:16 -0500

    add new sections

--- index.php
+++ index.php
@@ -35,15 +35,16 @@
                 aliquyam erat, sed diam voluptua. At vero
                 eos et accusam et justo duo dolores et ea
                 rebum. Stet clita kasd gubergren.</p>
-
-            <a href="#" class="btn mt-4">Lorem Ipsum Dolor</a>
-            <a href="#" class="btn secondary">Lorem Ipsum Dolor</a>
+            <div>
+                <a href="#" class="btn mt-4">Lorem Ipsum Dolor</a>
+                <a href="#" class="btn secondary">Lorem Ipsum Dolor</a>
+            </div>
         </div>
         <div class="hero-image-container">
             <img class="img-fluid w-100" src="<?php echo get_template_directory_uri() . '/assets/img/ar-at-typewriter-bw.png' ?>" alt="Ayn Rand Black & White Image">
         </div>
     </section>
-    <section class="margin-top">
+    <section class="funding-program margin-top">
         <div class="container-fluid">
             <div class="text-center">
                 <h2 class="section-header">
@@ -52,6 +53,142 @@
                 <p class="sub color-gray margin-top-half">
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo.
                 </p>
+            </div>
+            <div class="row margin-top">
+                <div class="col-md-4">
+                    <div class="square-card bg-light-orange">
+                        <h3 class="card-header">
+                            Objectivist<br />
+                            Venture Fund
+                        </h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="square-card bg-light-yellow">
+                        <h3 class="card-header">
+                            Scholarships<br />
+                            Program
+                        </h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="square-card bg-light-blue">
+                        <h3 class="card-header">
+                            Scholarships<br />
+                            Program
+                        </h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center mt-4">
+                <a href="#" class="btn btn-blue">Lorem Ipsum Dolor</a>
+                <a href="#" class="btn btn-blue-secondary">Lorem Ipsum Dolor</a>
+            </div>
+        </div>
+    </section>
+    <section class="about-prometheus margin-top bg-light-blue overflow-hidden pb-5">
+        <div class="about-prometheus-image-container">
+            <img class="img-fluid w-100" src="<?php echo get_template_directory_uri() . '/assets/img/PF Logo - Isolated.png' ?>" alt="Prometheus Torch Light Image">
+        </div>
+        <div class="about-prometheus-text">
+            <h2 class="color-blue">
+                About Prometheus Foundation
+            </h2>
+            <p>
+                Prometheus Foundation (PF) is an independent, non-profit enterprise whose mission is to actively promote Objectivism, the philosophy of Ayn Rand. To this end, PF seeks and funds proposals from individual Objectivists as well as from for-profit and non-profit organizations.
+            </p>
+            <p>
+                Prometheus Foundation operates with two funds: The Objectivist Venture Fund (OVF) and the New Intellectuals Fund (NIF). Carl Barney, a long-time advocate of Objectivist ideas, is the originator and sole funding source for Prometheus.
+            </p>
+            <p>
+                Objectivist Venture Fund was first announced at the Ayn Rand Institute’s 2014 Objectivist Summer Conference, and inaugurated shortly thereafter. OVF underwrites innovative projects whose aim is to spread Ayn Rand’s ideas with maximum impact.
+            </p>
+            <div class="text-center mt-3">
+                <a href="#" class="btn btn-blue">Lorem Ipsum Dolor</a>
+            </div>
+        </div>
+    </section>
+    <section class="what-we-fund margin-top pb-5">
+        <div class="container-fluid">
+            <div class="text-center">
+                <h2 class="section-header">
+                    What We Fund
+                </h2>
+                <p class="sub color-gray margin-top-half">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo.
+                </p>
+            </div>
+            <div class="row margin-top">
+                <div class="col-md-4">
+                    <div class="circle-card bg-light-orange">
+                        <h3 class="card-header text-left">
+                            Objectivist<br />
+                            Venture Fund
+                        </h3>
+                        <h5 class="text-uppercase">
+                            Proposal by Maria Marty
+                        </h5>
+                        <p>
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="circle-card bg-light-yellow">
+                        <h3 class="card-header text-left">
+                            Scholarships<br />
+                            Program
+                        </h3>
+                        <h5 class="text-uppercase">
+                            Proposal by Maria Marty
+                        </h5>
+                        <p>
+                            Lorem ipsum dolor sit amet, nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="circle-card bg-light-blue">
+                        <h3 class="card-header text-left">
+                            Scholarships<br />
+                            Program
+                        </h3>
+                        <h5 class="text-uppercase">
+                            Proposal by Maria Marty
+                        </h5>
+                        <p>
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center mt-4">
+                <a href="#" class="btn btn-blue">Lorem Ipsum Dolor</a>
+                <a href="#" class="btn btn-blue-secondary">Lorem Ipsum Dolor</a>
+            </div>
+        </div>
+    </section>
+    <section class="get-started bg-blue margin-top">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4">
+                    <img class="img-fluid w-100" src="<?php echo get_template_directory_uri() . '/assets/img/corporate-image.jpg' ?>" alt="Women in discussing in front of laptop">
+                </div>
+
+                <div class="offset-md-1 col-md-6">
+                    <h2 class="top-dash section-header border-none">
+                        Let’s get started!
+                    </h2>
+                </div>
             </div>
         </div>
     </section>
