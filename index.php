12eab69a5e5912524a71ac6b6da5cb11299b3fac
Riadul Hoque <hoqueriadul@gmail.com>
2019-12-06 23:13:23 -0500

    add footer and other sections

--- index.php
+++ index.php
@@ -25,6 +25,7 @@
 <body>
     <section class="hero color-white bg-light-blue overflow-hidden">
         <div class="hero-text">
+            <span class="top-dash"></span>
             <h1 class="hero-header">
                 Objectivists:<br class="hero-br" />
                 Put your ideas to work.
@@ -45,7 +46,7 @@
         </div>
     </section>
     <section class="funding-program margin-top">
-        <div class="container-fluid">
+        <div class="container-xxl">
             <div class="text-center">
                 <h2 class="section-header">
                     Funding Program
@@ -117,8 +118,8 @@
             </div>
         </div>
     </section>
-    <section class="what-we-fund margin-top pb-5">
-        <div class="container-fluid">
+    <section class="what-we-fund margin-top">
+        <div class="container-xxl">
             <div class="text-center">
                 <h2 class="section-header">
                     What We Fund
@@ -130,44 +131,54 @@
             <div class="row margin-top">
                 <div class="col-md-4">
                     <div class="circle-card bg-light-orange">
-                        <h3 class="card-header text-left">
-                            Objectivist<br />
-                            Venture Fund
-                        </h3>
-                        <h5 class="text-uppercase">
-                            Proposal by Maria Marty
-                        </h5>
-                        <p>
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
-                        </p>
+                        <div class="circle-card-content">
+                            <h3 class="card-header text-left">
+                                Objectivist<br />
+                                Venture Fund
+                            </h3>
+                            <h5 class="small text-uppercase">
+                                Proposal by Maria Marty
+                            </h5>
+                            <p class="small">
+                                This Proposal Establishes An Ayn Rand Center For
+                                Latin America, Headquartered In Buenos Aires.
+                                Activities Will Include Website, Conferences,
+                                Courses, And Translations Of Objectivist Work
+                                Into Spanish.
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="circle-card bg-light-yellow">
-                        <h3 class="card-header text-left">
-                            Scholarships<br />
-                            Program
-                        </h3>
-                        <h5 class="text-uppercase">
-                            Proposal by Maria Marty
-                        </h5>
-                        <p>
-                            Lorem ipsum dolor sit amet, nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
-                        </p>
+                        <div class="circle-card-content">
+                            <h3 class="card-header text-left">
+                                Scholarships<br />
+                                Program
+                            </h3>
+                            <h5 class="small text-uppercase">
+                                Proposal by Maria Marty
+                            </h5>
+                            <p class="small">
+                                Lorem ipsum dolor sit amet, nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="circle-card bg-light-blue">
-                        <h3 class="card-header text-left">
-                            Scholarships<br />
-                            Program
-                        </h3>
-                        <h5 class="text-uppercase">
-                            Proposal by Maria Marty
-                        </h5>
-                        <p>
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
-                        </p>
+                        <div class="circle-card-content">
+                            <h3 class="card-header text-left">
+                                Scholarships<br />
+                                Program
+                            </h3>
+                            <h5 class="small text-uppercase">
+                                Proposal by Maria Marty
+                            </h5>
+                            <p class="small">
+                                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -178,20 +189,46 @@
         </div>
     </section>
     <section class="get-started bg-blue margin-top">
-        <div class="container-fluid">
-            <div class="row">
+        <div class="container-fluid px-0">
+            <div class="row no-gutters">
                 <div class="col-md-4">
                     <img class="img-fluid w-100" src="<?php echo get_template_directory_uri() . '/assets/img/corporate-image.jpg' ?>" alt="Women in discussing in front of laptop">
                 </div>
-
                 <div class="offset-md-1 col-md-6">
-                    <h2 class="top-dash section-header border-none">
-                        Let’s get started!
-                    </h2>
+                    <div class="px-3">
+                        <span class="top-dash margin-top"></span>
+                        <h2 class="section-header color-white border-none">
+                            Let’s get started!
+                        </h2>
+                        <p class="color-white">
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.
+                        </p>
+                        <div class="text-center margin-top margin-bottom">
+                            <a href="#" class="btn">Lorem Ipsum Dolor</a>
+                            <a href="#" class="btn secondary">Lorem Ipsum Dolor</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
+    <footer class="footer bg-gray">
+        <div class="container-xxl">
+            <div class="d-flex justify-content-md-between justify-content-center flex-column flex-md-row align-items-md-baseline">
+                <div class="footer-logo-container">
+                    <img class="w-100 img-fluid" src="<?php echo get_template_directory_uri() . '/assets/img/Group%2023.png' ?>" alt="">
+                </div>
+                <ul class="footer-links d-md-flex justify-content-center align-items-md-baseline flex-wrap text-center margin-bottom">
+                    <li class="footer-link"><a href="">Home</a></li>
+                    <li class="footer-link"><a href="">About</a></li>
+                    <li class="footer-link"><a href="">What We Fund</a></li>
+                    <li class="footer-link"><a href="">FAQ</a></li>
+                    <li class="footer-link"><a href="">Contact</a></li>
+                    <li class="footer-link primary"><a href="">Apply</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
     <?php wp_footer(); ?>
 </body>
 
