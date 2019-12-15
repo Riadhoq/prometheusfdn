cf62de6f6c462bd96e5fed8392b15ff8c51ce0fe
Riadul Hoque <hoqueriadul@gmail.com>
2019-12-14 22:52:05 -0500

    add what we fund page

--- page-what-we-fund.php
+++ page-what-we-fund.php
@@ -2,6 +2,234 @@
 
 <?php get_header() ?>
 
-What we fund Page
+<section class="what-we-fund-hero hero color-white bg-light-blue overflow-hidden">
+    <div class="hero-container">
+        <div class="hero-text">
+            <span class="top-dash"></span>
+            <h1 class="hero-header">
+                What We Fund
+            </h1>
+            <p class="sub">Lorem ipsum dolor sit amet, consetetur
+                sadipscing elitr, sed diam nonumy eirmod
+                tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero
+                eos et accusam et justo duo dolores et ea
+                rebum. Stet clita kasd gubergren.</p>
+            <div class="hero-btn-group d-lg-flex">
+                <a href="#" class="btn mt-4">Lorem Ipsum Dolor</a>
+            </div>
+        </div>
+        <div class="hero-image-container">
+            <img class="img-fluid w-100" src="<?php echo get_template_directory_uri() . '/assets/img/casual-biz-meeting.jpg' ?>" alt="Business Casual meeting">
+        </div>
+    </div>
+</section>
 
-<?php get_footer() ?>
+<section class="margin-top margin-bottom">
+    <div class="container-xxl">
+        <div class="text-center">
+            <h2 class="section-header color-blue">
+                Objectivist Venture Fund
+            </h2>
+            <p class="sub color-gray margin-top-half">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo.
+            </p>
+        </div>
+        <div class="row margin-top justify-content-center">
+            <div class="col-md-6 col-lg-4">
+                <div class="circle-card bg-light-orange">
+                    <div class="circle-card-content">
+                        <h3 class="card-header text-left">
+                            Objectivist<br />
+                            Venture Fund
+                        </h3>
+                        <h5 class="small text-uppercase">
+                            Proposal by Maria Marty
+                        </h5>
+                        <p class="small">
+                            This Proposal Establishes An Ayn Rand Center For
+                            Latin America, Headquartered In Buenos Aires.
+                            Activities Will Include Website, Conferences,
+                            Courses, And Translations Of Objectivist Work
+                            Into Spanish.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="circle-card bg-light-orange">
+                    <div class="circle-card-content">
+                        <h3 class="card-header text-left">
+                            Scholarships<br />
+                            Program
+                        </h3>
+                        <h5 class="small text-uppercase">
+                            Proposal by Maria Marty
+                        </h5>
+                        <p class="small">
+                            Lorem ipsum dolor sit amet, nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="circle-card bg-light-orange">
+                    <div class="circle-card-content">
+                        <h3 class="card-header text-left">
+                            Scholarships<br />
+                            Program
+                        </h3>
+                        <h5 class="small text-uppercase">
+                            Proposal by Maria Marty
+                        </h5>
+                        <p class="small">
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="margin-top margin-bottom pt-5">
+    <div class="container-xxl">
+        <div class="text-center">
+            <h2 class="section-header color-blue">
+                Scholarships Program
+            </h2>
+            <p class="sub color-gray margin-top-half">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo.
+            </p>
+        </div>
+        <div class="row margin-top justify-content-center">
+            <div class="col-md-6 col-lg-4">
+                <div class="circle-card bg-light-yellow">
+                    <div class="circle-card-content">
+                        <h3 class="card-header text-left">
+                            Objectivist<br />
+                            Venture Fund
+                        </h3>
+                        <h5 class="small text-uppercase">
+                            Proposal by Maria Marty
+                        </h5>
+                        <p class="small">
+                            This Proposal Establishes An Ayn Rand Center For
+                            Latin America, Headquartered In Buenos Aires.
+                            Activities Will Include Website, Conferences,
+                            Courses, And Translations Of Objectivist Work
+                            Into Spanish.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="circle-card bg-light-yellow">
+                    <div class="circle-card-content">
+                        <h3 class="card-header text-left">
+                            Scholarships<br />
+                            Program
+                        </h3>
+                        <h5 class="small text-uppercase">
+                            Proposal by Maria Marty
+                        </h5>
+                        <p class="small">
+                            Lorem ipsum dolor sit amet, nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="circle-card bg-light-yellow">
+                    <div class="circle-card-content">
+                        <h3 class="card-header text-left">
+                            Scholarships<br />
+                            Program
+                        </h3>
+                        <h5 class="small text-uppercase">
+                            Proposal by Maria Marty
+                        </h5>
+                        <p class="small">
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="margin-top pt-4 margin-bottom">
+    <div class="container-xxl">
+        <div class="text-center">
+            <h2 class="section-header color-blue">
+                Productiveness Program
+            </h2>
+            <p class="sub color-gray margin-top-half">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo.
+            </p>
+        </div>
+        <div class="row margin-top justify-content-center">
+            <div class="col-md-6 col-lg-4">
+                <div class="circle-card bg-light-blue">
+                    <div class="circle-card-content">
+                        <h3 class="card-header text-left">
+                            Objectivist<br />
+                            Venture Fund
+                        </h3>
+                        <h5 class="small text-uppercase">
+                            Proposal by Maria Marty
+                        </h5>
+                        <p class="small">
+                            This Proposal Establishes An Ayn Rand Center For
+                            Latin America, Headquartered In Buenos Aires.
+                            Activities Will Include Website, Conferences,
+                            Courses, And Translations Of Objectivist Work
+                            Into Spanish.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="circle-card bg-light-blue">
+                    <div class="circle-card-content">
+                        <h3 class="card-header text-left">
+                            Scholarships<br />
+                            Program
+                        </h3>
+                        <h5 class="small text-uppercase">
+                            Proposal by Maria Marty
+                        </h5>
+                        <p class="small">
+                            Lorem ipsum dolor sit amet, nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="circle-card bg-light-blue">
+                    <div class="circle-card-content">
+                        <h3 class="card-header text-left">
+                            Scholarships<br />
+                            Program
+                        </h3>
+                        <h5 class="small text-uppercase">
+                            Proposal by Maria Marty
+                        </h5>
+                        <p class="small">
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<?php
+get_template_part("template-parts/get-started-banner");
+get_footer();
+
+?>
