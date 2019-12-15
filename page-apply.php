83e640fa17fb3380e4fa5ff5c2cd8ef8e18c7424
Riadul Hoque <hoqueriadul@gmail.com>
2019-12-15 00:15:13 -0500

    application page mobile

--- page-apply.php
+++ page-apply.php
@@ -2,6 +2,31 @@
 
 <?php get_header() ?>
 
-Apply Page
+<section class="hero color-white bg-light-blue overflow-hidden">
+    <div class="container-xxl hero-container">
+        <div class="hero-text">
+            <span class="top-dash"></span>
+            <h1 class="hero-header">
+                Submit your application&nbsp;today
+            </h1>
+            <p class="sub">Lorem ipsum dolor sit amet, consetetur
+                sadipscing elitr, sed diam nonumy eirmod
+                tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero
+                eos et accusam et justo duo dolores et ea
+                rebum. Stet clita kasd gubergren.</p>
+            <div class="hero-btn-group d-lg-flex">
+                <a href="#" class="btn mt-4">Lorem Ipsum Dolor</a>
+            </div>
+        </div>
+        <div class="hero-image-container application-hero-text color-black text-uppercase">
+            <h5>Choose a funding program</h5>
+            <h5>Fill out the application</h5>
+            <h5>Check your email</h5>
+        </div>
+    </div>
+</section>
+
+<?php get_template_part("template-parts/frequently-asked-questions") ?>
 
 <?php get_footer() ?>
--- page-contact.php
+++ page-contact.php
@@ -2,6 +2,19 @@
 
 <?php get_header() ?>
 
-Contact Page
+<section class="margin-top margin-bottom">
+    <div class="text-center">
+        <h2 class="section-header">
+            Contact Us
+        </h2>
+        <p class="sub color-gray margin-top-half">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo.
+        </p>
+    </div>
+
+    <div class="container">
+        Form
+    </div>
+</section>
 
 <?php get_footer() ?>
