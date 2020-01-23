19f5d30049312653479422d6de5733886d145a44
Riadul Hoque <hoqueriadul@gmail.com>
2020-01-22 19:22:49 -0500

    add custom page feature

---
+++ page.php
@@ -0,0 +1,27 @@
+<?php /*Page Name: Faq*/
+
+get_header();
+
+// Start the loop.
+while (have_posts()) : the_post();
+
+    /*
+     * Include the post format-specific template for the content. If you want to
+     * use this in a child theme, then include a file called called content-___.php
+     * (where ___ is the post format) and that will be used instead.
+     */
+?>
+    <div class="container">
+        <?php
+        if (has_post_thumbnail()) {
+            the_post_thumbnail();
+        }
+        the_content();
+        ?>
+    </div>
+<?php
+// End the loop.
+endwhile;
+get_footer();
+
+?>
